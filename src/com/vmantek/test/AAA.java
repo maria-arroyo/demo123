86984ec5c22d79ec18cc8450405ca6cf3a679432
Maria Angelica Arroyo de Leon <maria.arroyo@neoris.com>
2012-05-24 19:23:42 -0500

    Se agrego clase AAA

---
+++ src/com/vmantek/test/AAA.java
@@ -0,0 +1,11 @@
+package com.vmantek.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maria.arroyo
+ * Date: 5/24/12
+ * Time: 7:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AAA {
+}
